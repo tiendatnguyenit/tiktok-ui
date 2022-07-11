700effcc781de4c0831aec60c75fd4afc9d5e1bf
tiendatnguyenit <tiendatnguyenit@gmail.com>
2022-07-11 09:00:34 +0700

    update

---
+++ src/components/Layout/DefaultLayout/index.js
@@ -0,0 +1,18 @@
+import Header from './Header'
+import Sidebar from './SideBar'
+
+function DefaultLayout({children}) {
+    return (
+        <div>
+            <Header />
+            <div className='container'>
+                <Sidebar />
+                <div className='content'>
+                    {children}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default DefaultLayout;
---
+++ src/routes/index.js
@@ -0,0 +1,18 @@
+import Home from '~/pages/Home'
+import Following from '~/pages/Following'
+import Profile from '~/pages/Profile'
+
+//publicRoutes
+const publicRoutes = [
+    { path: '/', component: Home},
+    { path: '/following', component: Following},
+    { path: '/profile', component: Profile},
+]
+
+const privateRoutes = [
+
+]
+
+export { publicRoutes, privateRoutes };
+
+
